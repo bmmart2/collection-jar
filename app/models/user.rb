0092af0f38c184e1eeef837bbd38e4a0e15396d1
Brandon M <bmmart2@gmail.com>
2019-03-01 00:12:56 -0600

    Require first and last name on registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :videogames, dependent: :destroy
+  validates :fname, :lname, :presence => true
 
 end
