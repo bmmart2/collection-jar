6766d8137842bfa50e99f17dcb66bb1d1c5f0989
steve4835 <stevedaniel4835@gmail.com>
2019-03-26 20:55:08 -0500

    more sendgrid setup

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,5 +1,5 @@
 #config/initializers/sendgrid.rb
 
 $smtp_username = ENV["sendgrid_smtp_username"]
-$smtp_password = ENV["sendgrid_smtp_username"]
+$smtp_password = ENV["sendgrid_smtp_password"]
 $smtp_port = ENV["sendgrid_smtp_port"]
