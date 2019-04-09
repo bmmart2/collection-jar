d62ac4653edbdc2afca745dfdd2aba7a1500d12b
planteater8 <bimster9@gmail.com>
2019-04-08 20:20:13 -0500

    Im really bad at git... schema fixed

--- db/schema.rb
+++ db/schema.rb
@@ -29,15 +29,15 @@ ActiveRecord::Schema.define(version: 2019_04_09_005140) do
   end
 
   create_table "movies", force: :cascade do |t|
-  t.string "title"
-  t.string "year"
-  t.string "genre"
-  t.datetime "created_at", null: false
-  t.datetime "updated_at", null: false
-  t.integer "user_id"
-  t.index ["user_id"], name: "index_movies_on_user_id"
-  t.index [nil], name: "index_movies_on_user"
-end
+    t.string "title"
+    t.string "year"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_movies_on_user_id"
+    t.index [nil], name: "index_movies_on_user"
+  end
 
   create_table "uploads", force: :cascade do |t|
     t.string "title"
