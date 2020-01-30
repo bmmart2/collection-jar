1f2a2ad5e49f1a25850edb2a568af1700bbe29fc
Brandon Martinez <bmmart2@gmail.com>
2020-01-29 23:11:09 -0600

    update stuff that doesnt need to be here

--- test/controllers/login_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LoginControllerTest < ActionDispatch::IntegrationTest
-  test "should get signup" do
-    get login_signup_url
-    assert_response :success
-  end
-
-end
